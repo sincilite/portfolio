b922bc86dcd985cedb301a1642fd339b9846315f
Michael van Rooyen <michael.vanrooyen@madebyextreme.com>
2021-11-14 14:25:18 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "portfolio",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-datocms",
+      options: {
+        apiToken: "018e3c935617e74809a52bbc2ca982",
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
